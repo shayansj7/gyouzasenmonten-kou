59f52d6534b70c6414c5b4c5c9d3602ca1401a7b
shayansj7 <shayansj777@gmail.com>
2025-01-20 05:01:49 -0500

    Updated carousel images' style

--- components/ExampleCarouselImage.jsx
+++ components/ExampleCarouselImage.jsx
@@ -1,10 +1,14 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function ExampleCarouselImage({ text }) {
   return (
-    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+    <div
+      className={`carousel-image-container ${text
+        .toLowerCase()
+        .replace(/\s+/g, "-")}`}
+    >
       <Image
-        src={`/images/${text.toLowerCase().replace(/\s+/g, '-')}.jpg`} 
+        src={`/images/${text.toLowerCase().replace(/\s+/g, "-")}.jpg`}
         alt={text}
         layout="fill"
         objectFit="cover"
