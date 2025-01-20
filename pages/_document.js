90243d1217ed6dfe55561805d1a31cd3f2c7fe2c
shayansj7 <shayansj777@gmail.com>
2025-01-20 14:35:51 -0500

    Updated title and meta for better SEO.

--- pages/_document.js
+++ pages/_document.js
@@ -15,10 +15,6 @@ export default function Document() {
           name="keywords"
           content="餃, 餃子, Kou, レストラン, 日本食, 餃子店, gyoza, restaurant, dumpling"
         />
-        <meta
-          name="description"
-          content="餃（KOU）は、埼玉県所沢市の餃子専門店です。 餃子愛溢れるスタッフが、毎日の仕入れ、仕込みを行っています。是非ご賞味ください。 餃（KOU）の餃子はお持ち帰り・全国発送も承っています。 お土産にも最適です。"
-        />
 
         {/* Open Graph Meta Tags for Social Media */}
         <meta property="og:type" content="website" />
--- pages/en/index.js
+++ pages/en/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Container, Row, Col } from "react-bootstrap";
 import ControlledCarousel from "@/components/Carousels";
 import { Details } from "@/components/Details";
@@ -7,6 +8,16 @@ import News from "@/components/News";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Gyoza Senmonten Kou</title>
+        <meta
+          name="description"
+          content="餃（KOU）は、埼玉県所沢市の餃子専門店です。
+                    餃子愛溢れるスタッフが、毎日の仕入れ、仕込みを行っています。是非ご賞味ください。
+                    餃（KOU）の餃子はお持ち帰り・全国発送も承っています。
+                    お土産にも最適です。"
+        />
+      </Head>
       <ControlledCarousel />
       <Container fluid className="index-container">
         <Container className="my-4">
