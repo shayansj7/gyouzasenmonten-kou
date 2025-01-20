eb2ed7e4fb5491e105d0d5f321484b33256136b0
shayansj7 <shayansj777@gmail.com>
2025-01-20 04:46:57 -0500

    Updated header and home page for better SEO.

--- components/Details.jsx
+++ components/Details.jsx
@@ -1,32 +1,41 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { Row, Col, Container } from "react-bootstrap";
 
 export function Details() {
+  const router = useRouter();
+  const isEnglish = router.pathname.startsWith("/en");
 
-    const router = useRouter();
-    const isEnglish = router.pathname.startsWith('/en');
-
-    return (
-        <>
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col sm={4}>
             <Image
-                src="/images/gyoza-senmonten-kou.png"
-                alt="Gyoza-Senmonten-kou"
-                className="img-fluid"
-                width={400}
-                height={300}
+              src="/images/og-image.jpg"
+              alt="Gyoza-Senmonten-kou"
+              className="img-fluid"
+              width={400}
+              height={300}
             />
-            <h5>
-                {
-                    isEnglish ? `餃(KOU) is a gyoza specialty shop located in Tokorozawa City, Saitama Prefecture.
+          </Col>
+          <Col sm={8} className="details-desc">
+            <div>
+              <h5>
+                {isEnglish
+                  ? `餃(KOU) is a gyoza specialty shop located in Tokorozawa City, Saitama Prefecture.
                     Our staff, passionate about gyoza, handle daily sourcing and preparation with care. Please give them a try!
                     餃(KOU) offers gyoza for takeout and nationwide shipping.
                     They are perfect as gifts!`
-                    : `餃（KOU）は、埼玉県所沢市の餃子専門店です。
+                  : `餃（KOU）は、埼玉県所沢市の餃子専門店です。
                     餃子愛溢れるスタッフが、毎日の仕入れ、仕込みを行っています。是非ご賞味ください。
                     餃（KOU）の餃子はお持ち帰り・全国発送も承っています。
-                    お土産にも最適です。`
-                }
-            </h5>
-        </>
-    );
+                    お土産にも最適です。`}
+              </h5>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,38 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        {/* Global Meta Tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Shayan Shimura" />
+        <meta
+          name="keywords"
+          content="餃, 餃子, Kou, レストラン, 日本食, 餃子店, gyoza, restaurant, dumpling"
+        />
+        <meta
+          name="description"
+          content="餃（KOU）は、埼玉県所沢市の餃子専門店です。 餃子愛溢れるスタッフが、毎日の仕入れ、仕込みを行っています。是非ご賞味ください。 餃（KOU）の餃子はお持ち帰り・全国発送も承っています。 お土産にも最適です。"
+        />
+
+        {/* Open Graph Meta Tags for Social Media */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="餃子Kou | 本格的な餃子レストラン"
+        />
+        <meta
+          property="og:description"
+          content="餃（KOU）は、埼玉県所沢市の餃子専門店です。 餃子愛溢れるスタッフが、毎日の仕入れ、仕込みを行っています。是非ご賞味ください。 餃（KOU）の餃子はお持ち帰り・全国発送も承っています。 お土産にも最適です。"
+        />
+        <meta property="og:url" content="https://gyozasenmonten-kou.jp" />
+        <meta
+          property="og:image"
+          content="images/og-image.jpg"
+        />
       </Head>
       <body>
         <Main />
